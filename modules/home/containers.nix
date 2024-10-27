e68edf8c97fc70aeb43f5a8c5154db2b43fd0ea8
Edgar Lee <edgarhinshunlee@gmail.com>
2024-10-27 18:42:17 +0800

    refactor: Restructure containers.nix for better modularity

--- modules/home/containers.nix
+++ modules/home/containers.nix
@@ -5,28 +5,30 @@ with lib;
 let
   cfg = config.home.containers;
 
-in {
-  options.home.containers = {
-    enable = mkEnableOption "container support";
+in mkMerge [
+  {
+    options.home.containers = {
+      enable = mkEnableOption "container support";
 
-    type = mkOption {
-      type = types.enum [ "rootless" "rootful" ];
-      default = "rootless";
-      description = "Whether to use rootless or rootful containers";
+      type = mkOption {
+        type = types.enum [ "rootless" "rootful" ];
+        default = "rootless";
+        description = "Whether to use rootless or rootful containers";
+      };
     };
-  };
 
-  config = mkIf cfg.enable (mkMerge [
+    config = mkIf cfg.enable {
+      home.packages = with pkgs; [
+        nerdctl
+      ];
+    };
+  }
+  (mkIf cfg.enable (mkMerge [
     (mkIf (cfg.type == "rootless") {
       imports = [ ./rootless-containers.nix ];
     })
     (mkIf (cfg.type == "rootful") {
       imports = [ ./rootful-containers.nix ];
     })
-    {
-      home.packages = with pkgs; [
-        nerdctl
-      ];
-    }
-  ]);
-}
+  ]))
+]
